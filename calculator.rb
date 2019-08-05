0832178f3699009b187cf42f9ac646b4c93a2861
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-05 14:16:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,23 @@
 # Add your variables here
+first_number = 20
+second_number = 10 
+
+def sum
+  sum = (first_number + second_number)
+  puts sum
+end 
+
+def difference 
+  difference = (first_number - second_number)
+  puts difference 
+end 
+
+def product 
+  product = (first_number * second_number)
+  puts product
+end 
+
+def quotient
+  quotient = (first_number / second_number)
+  puts quotient
+end 
